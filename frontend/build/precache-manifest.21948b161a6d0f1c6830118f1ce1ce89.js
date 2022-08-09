1e70f217facaab2d907d01ca432c6afc840be6fa
UpskillProject <102297543+UpskillProject@users.noreply.github.com>
2022-08-09 20:49:17 +0100

    Add files via upload

---
+++ frontend/build/precache-manifest.21948b161a6d0f1c6830118f1ce1ce89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbca0692ab429ac6bbd4497e2c11f7df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c29414e628416ca426fe",
+    "url": "/static/css/main.19a9c7e3.chunk.css"
+  },
+  {
+    "revision": "ddd2516606fb8d89433e",
+    "url": "/static/js/2.085612ba.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.085612ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c29414e628416ca426fe",
+    "url": "/static/js/main.aff49bb9.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
